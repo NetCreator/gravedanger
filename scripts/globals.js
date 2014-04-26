d019867962ef3fa45f1bc7c99ead11ebb39ae708
Mark Smullen <mark@techfocus.net>
2014-04-26 18:42:45 -0500

    Pull globals from master

--- scripts/globals.js
+++ scripts/globals.js
@@ -9,5 +9,5 @@ var globals = {
     /**
      * These are the game states our game may operate in.
      */
-    var gameState = {MAINMENU : 1, PLAYING: 2, GAMEOVER: 3};
-}
+    gameState :  {MAINMENU : 1, PLAYING: 2, GAMEOVER: 3}
+};

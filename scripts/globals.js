f6ab8787ab193591f7902c8f931c991f474a3490
Mark Smullen <mark@techfocus.net>
2014-04-26 16:39:10 -0500

    Set up our globals js

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,1 +1,13 @@
-var gameState = {MAINMENU : 1, PLAYING: 2, GAMEOVER: 3};
+/**
+ * @fileOverview Our global constants and definitions.
+ * @author Texnical, LeyarotheConqueror, AlejandorLazaro
+ * @version 0.1a
+ */
+
+/** @namespace  */
+var globals = {
+    /**
+     * These are the game states our game may operate in.
+     */
+    var gameState = {MAINMENU : 1, PLAYING: 2, GAMEOVER: 3};
+}

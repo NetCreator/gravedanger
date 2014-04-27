d6271b804ee0f063ab79706974d022b6f2bc4041
leyarotheconquerer <HazenJohnson@letu.edu>
2014-04-27 16:20:32 -0500

    Add additive drawing for the layer.

--- scripts/layer.js
+++ scripts/layer.js
@@ -3,15 +3,15 @@ function layer(game, imgkey)
     this.game = game;
     
     // Size of the physical tile image
-    this.actualTileSize = 64;
+    this.actualTileSize = 128;
     
     // Size the game treats the tile as for purposes of rendering larger than a tile
-    this.virtualTileSize = 50;
+    this.virtualTileSize = 100;
     
     // Dynamically determine the dimensions of the logic grid
     var logicGridStats = {
-        numRows: Math.floor(800/this.virtualTileSize),
-        numColumns: Math.floor(600/this.virtualTileSize)
+        numRows: Math.floor(800/this.virtualTileSize)+3,
+        numColumns: Math.floor(600/this.virtualTileSize)+3
     };
     
     this.logicGrid = new Array(logicGridStats.numRows);
@@ -19,6 +19,12 @@ function layer(game, imgkey)
     // Logic grid creation
     for (var i = 0; i < logicGridStats.numRows; i++) {
         this.logicGrid[i] = new Array(logicGridStats.numColumns);
+        
+        // Junk init loop
+        for (var j = 0; j < this.logicGrid[i].length; j++)
+        {
+            this.logicGrid[i][j] = 0;
+        }
     }
     
     this.patches;
@@ -36,14 +42,42 @@ function layer(game, imgkey)
             this.patches[i] = new Array(this.logicGrid[0].length);
         }
         
+        var tilepatch;
+        var alphapatch;
+        
         // Generate the dirt patch for each logical tile
-        for(y = 0; y < this.logicGrid.length; y++)
+        for(var y = 0; y < this.logicGrid.length; y++)
         {
-            for(x = 0; x <= this.logicGrid[y].length; x++)
+            for(var x = 0; x <= this.logicGrid[y].length; x++)
             {
-                if(this.grid[y][x] == 0)
+                if(this.logicGrid[y][x] == 0)
                 {
+                    // Create the bitmap data structure for storing the tile patch
+                    tilepatch = game.add.bitmapData(
+                        this.actualTileSize,
+                        this.actualTileSize, 
+                        '', false);
                     
+                    // Copy the tile patch from the indicated source image
+                    tilepatch.copyPixels(imgkey,
+                        new Phaser.Rectangle(
+                            (x-1)*this.virtualTileSize,
+                            (y-1)*this.virtualTileSize,
+                            this.actualTileSize,
+                            this.actualTileSize
+                        ),0,0);
+
+                    // Create the bitmap data structure for storing the masked tile patch
+                    alphapatch = game.add.bitmapData(this.actualTileSize, this.actualTileSize, 'alphapatch', true);
+                    
+                    // Mask the tile using the given mask
+                    alphapatch.alphaMask(tilepatch.canvas, 'mask');
+                    
+                    // Add the masked spirte
+                    this.patches[y][x] = game.add.sprite(
+                        (x-1)*this.virtualTileSize - (this.actualTileSize - this.virtualTileSize)/2,
+                        (y-1)*this.virtualTileSize - (this.actualTileSize - this.virtualTileSize)/2,
+                        alphapatch);
                 }
             }
         }
--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -11,7 +11,7 @@ globals.gameState.MAINMENU = function (game) {
         // Load the background for the main menu
         game.load.image('menubackground', 'images/TitleMenu/MenuLayout.png');
         game.load.image('startbutton',    'images/TitleMenu/StartButton.png');
-        //game.load.audio('menuBGM', 'music/creepyloop.wav');
+        game.load.audio('menuBGM', 'music/creepyloop.wav');
     };
 
     this.create = function () {
@@ -21,7 +21,7 @@ globals.gameState.MAINMENU = function (game) {
         // Create a start button and render
         game.add.button(offsets.startbutton.x, offsets.startbutton.y, 'startbutton', this.startGame);
         
-        //music = game.add.audio('menuBGM', 1, true);
+        music = game.add.audio('menuBGM', 1, true);
         //music.play('',0,1,true);
     };
 

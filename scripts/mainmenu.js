461ecefb3291f4f6f3fc5aaeff8072a98445a6d6
Sherlock <Nebilim95@gmail.com>
2014-04-26 19:47:47 -0500

    edited the pathing for the menu and buttons to reference the proper images

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -1,8 +1,8 @@
 globals.gameState.MAINMENU = function (game) {
     this.preload = function () {
         // Load the background for the main menu
-        game.load.image('menubackground', 'images/titlemenu/MenuLayout.png');
-        game.load.image('startbutton', 'images/startbutton.png');
+        game.load.image('menubackground', 'images/TitleMenu/MenuLayout.png');
+        game.load.image('startbutton', 'images/TitleMenu/StartButton.png');
     };
 
     this.create = function () {

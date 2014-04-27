02f08de8ec989eb416dd8b62282202fc928f6bf7
Sherlock <Nebilim95@gmail.com>
2014-04-26 23:38:47 -0500

    Accitdentally made the changes to the mainmenu.js on the master branch... whoops :P

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -14,6 +14,7 @@ globals.gameState.MAINMENU = function (game) {
         // Load the background for the main menu
         game.load.image('menubackground', 'images/TitleMenu/MenuLayout.png');
         game.load.image('startbutton', 'images/TitleMenu/StartButton.png');
+        game.load.image('title', 'images/TitleMenu/TitleText.png');
     };
 
     this.create = function () {
@@ -22,9 +23,9 @@ globals.gameState.MAINMENU = function (game) {
 
         // Create a start button and render
         game.add.button(offsets.startbutton.x, offsets.startbutton.y, 'startbutton', this.startGame);
-
-        // Create the title text and render it
-        game.add.text(offsets.titletext.x, offsets.titletext.y, "Grave Digger", {font: "fonts/Cracked.text", fill: "#ffffff"});
+        
+        // Render the title
+        game.add.sprite(68, 188, 'title');
     };
 
     this.update = function () {

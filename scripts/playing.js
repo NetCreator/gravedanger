c652a74904b20435b0198832088852c9720a2db3
Sherlock <Nebilim95@gmail.com>
2014-04-27 00:21:57 -0500

    Adding in the basic empty logic grid.

--- scripts/playing.js
+++ scripts/playing.js
@@ -1,6 +1,13 @@
 globals.gameState.PLAYING = function (game) {
     var layers;
-
+    
+    var logicGridStats = {
+        numRows: 100,
+        numCollumns: 100
+    };
+    
+    var logicGrid = new Array(logicGridStats.numRows);
+    
     this.preload = function () {
         game.load.audio('hittingcoffin', 'sounds/hitingcoffin.wav');
         game.load.audio('diggingdirt', 'sounds/digindirt.wav');
@@ -13,10 +20,15 @@ globals.gameState.PLAYING = function (game) {
             new layer(game, 'layer1'),
             new layer(game, 'layer2')
         ];
-
+        
         nextLayer = game.add.existing(layers[1].sprite);
         layer = game.add.existing(layers[0].sprite);
         game.input.onDown.add(this.nextLayer, this);
+        
+        // Logic grid creation
+        for (var i = 0; i < logicGridStats.numRows; i++) {
+            logicGrid[i] = new Array(logicGridStats.numCollumns);
+        }
     };
 
     this.update = function () {
@@ -24,6 +36,6 @@ globals.gameState.PLAYING = function (game) {
 
     this.nextLayer = function () {
         layer = nextLayer;
-        enxtLayer = null;
+        nextLayer = null;
     };
 };

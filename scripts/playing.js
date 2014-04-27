c6bc6081a8bd9b6a046d7e3b816511a33a78cc1c
leyarotheconquerer <HazenJohnson@letu.edu>
2014-04-26 19:58:08 -0500

    Make the playing state distinguishable.

--- scripts/playing.js
+++ scripts/playing.js
@@ -1,8 +1,10 @@
 globals.gameState.PLAYING = function (game) {
     this.preload = function () {
+        game.load.image('background', 'phaser_tutorial_02/assets/sky.png');
     };
 
     this.create = function () {
+        game.add.sprite(0,0,'background');
     };
 
     this.update = function () {

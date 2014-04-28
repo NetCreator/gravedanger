4b5701b8b7148d395bff91d13a9eac644831a8f8
Mark Smullen <mark@techfocus.net>
2014-04-28 07:14:53 -0500

    Changed text offset, added new text

--- scripts/intro.js
+++ scripts/intro.js
@@ -4,15 +4,20 @@ globals.gameState.INTRO = function (game) {
      * @type {Array}
      */
     var flavortext = {
-        key:         ['text1', 'text2', 'text3', 'text4', 'text5', 'text6', 'text7'],
-        offsetX:     [422/2, 486/2, 262/2, 120/2, 326/2, 174/2, 234/2],
-        fadeInTime:  [4, 5, 4, 3, 6, 4, 3],
-        fadeOutTime: [2, 2, 2, 2, 2, 2, 5]
+        key:         ['text1', 'text2', 'text3', 'text4', 'text5', 'text6', 'text7', 'text8'],
+        offsetX:     [422/2, 486/2, 262/2, 120/2, 112/2, 326/2, 174/2, 234/2],
+        fadeInTime:  [4, 5, 4, 3, 3, 6, 4, 3],
+        fadeOutTime: [2, 2, 2, 2, 2, 2, 2, 4]
     };
+
+    var textOffset = {
+        x: 400,
+        y: 200
+    }
 
     this.preload = function () {
         this.load.image('black', 'images/Intro/black.png');
-        //this.load.image('sight', 'images/layer/coffintop1.png');
+        this.load.image('sight', 'images/layers/coffintop1.png');
         this.load.image('text1', 'images/Intro/text1.png');
         this.load.image('text2', 'images/Intro/text2.png');
         this.load.image('text3', 'images/Intro/text3.png');
@@ -20,10 +25,12 @@ globals.gameState.INTRO = function (game) {
         this.load.image('text5', 'images/Intro/text5.png');
         this.load.image('text6', 'images/Intro/text6.png');
         this.load.image('text7', 'images/Intro/text7.png');
+        this.load.image('text8', 'images/Intro/text8.png');
     };
 
     this.create = function () {
-        this.add.sprite(0, 0, 'black');
+        this.bgGroup = this.add.group();
+        this.bgGroup.create(0, 0, 'black');
 
         this.textGroup = this.add.group();
         this.textGroup.alpha = 0;
@@ -41,7 +48,7 @@ globals.gameState.INTRO = function (game) {
         }
 
         this.textGroup.remove(this.curtext, true);
-        this.curtext = this.textGroup.create(400 - flavortext.offsetX[++this.nextText], 150, flavortext.key[this.nextText]);
+        this.curtext = this.textGroup.create(textOffset.x - flavortext.offsetX[++this.nextText], textOffset.y, flavortext.key[this.nextText]);
 
         var tween = this.add.tween(this.textGroup);
         tween.to({alpha: 1}, flavortext.fadeInTime[this.nextText] * 1000);
